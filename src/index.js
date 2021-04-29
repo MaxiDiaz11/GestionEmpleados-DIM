e6049980ba2e73fdc660f05494ea2079fbf59449
Maxi Diaz <maxi.diaz66@gmail.com>
2021-04-29 12:15:28 -0300

    Se agregaron modificacions al JSON modelo

--- src/index.js
+++ src/index.js
@@ -1,22 +1,22 @@
-const express = require('express');
-const morgan = require('morgan');
-const client = require('../data/ConexionDB');
+const express = require("express");
+const morgan = require("morgan");
+const client = require("../data/ConexionDB");
 
 //crear una app de express
 const app = express();
 
-app.set('port', process.env.PORT || 3000);
+app.set("port", process.env.PORT || 3000);
 
-app.use(morgan('dev'));
-app.use(express.urlencoded({extended: false}));
+app.use(morgan("dev"));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // <- USO DE RUTAS ->
 // ruta codigoPersonal
-app.use('/personal/CodigoPersonal', require('../routes/codigoEmpleado_ruta'));
-app.use('/empleado', require('../routes/empleado_ruta'));
-app.use('/informeDiario', require('../routes/informeDiario_ruta'));
+app.use("/personal/CodigoPersonal", require("../routes/codigoEmpleado_ruta"));
+app.use("/empleado", require("../routes/empleado_ruta"));
+app.use("/informeDiario", require("../routes/informeDiario_ruta"));
 
-app.listen(app.get('port'), () => {
-  console.log(`Server on port ${app.get('port')}`);
+app.listen(app.get("port"), () => {
+  console.log(`Server on port ${app.get("port")}`);
 });
